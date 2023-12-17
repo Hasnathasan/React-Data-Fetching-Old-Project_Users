0b54355d45d8b5481f41139c9b20cf6231ea843d
Hasnathasan <hasnatoooooooo@gmail.com>
2023-12-17 17:21:38 +0600

    Code formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Users from './assets/Components/Users/Users'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Users from "./assets/Components/Users/Users";
 
 function App() {
   const [data, setData] = useState([]);
-  useEffect( () => {
-    fetch('data.json')
-      .then(res => res.json())
-      .then(data => setData(data))
-  },[]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
   const showId = (id) => {
     console.log(id);
-  }
+  };
   return (
     <div className="App">
-      {
-        data.map(eachData => 
-          <div>
-            <Users></Users>
-          </div>
-        
-        )
-      }
+      {data.map((eachData) => (
+        <div>
+          <Users></Users>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
